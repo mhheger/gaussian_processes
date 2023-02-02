f549f3ed6f4e6eec87bb3a970f51e661a4beddec
Manuel Heger <mh.heger@gmail.com>
2023-02-02 20:35:26 +0100

    first approach for the algorithm 2.1 it's allready calculating stuff, that seems to be possible. But there are still some sanity checks to do. As an optimization, we might do it more objective. There are some matrices, we don't need to calculate everything... Mein Englisch ist echt grottig...

---
+++ algorithm_2.1.R
@@ -0,0 +1,54 @@
+predict_gauss <- function(X_learn, y_learn, kov, noise, x_input){
+  #do some converting to get either atomic vectors or list of vectors
+  X_learn <- convert_to_list(X_learn, length(y_learn))
+  #stopifnot((length(x_input)==length(X_learn[[1]])) = "Function can only handle one input vector"))
+  #need: add sanity checks for kov, input sizes and types, handle different type
+  # of input data
+  
+  #these values can be stored in a attribut of the gaussian process, they just
+  #depend on the learning data
+  K <- kov_cross(X_learn, X_learn, kov)
+  L <- chol(K + diag(x=noise, nrow = nrow(K)), pivot = TRUE)
+  alpha <- solve(t(L))%*%solve(L)%*%y_learn
+  
+  k_1 <- kov_cross(X_learn,x_input, kov) 
+  f_predict <- t(k_1)%*%alpha
+  
+  v <- solve(L) %*% k_1 
+  
+  var_f <- kov(x_input,x_input)-sum(v^2)
+  
+  log_marginal_likelihood <- -0.5* t(y_learn) %*% alpha - sum(log(diag(L)))-nrow(L)*0.5*2*pi
+  
+  results <- list("f_predict" = f_predict,
+                  "var_f" = var_f,
+                  "log_marginal_likelihood"= log_marginal_likelihood)
+  return(results)
+}
+
+
+convert_to_list <- function(x, n){
+  if(length(x)-n>0 & !is.list(x)){        
+    if(is.matrix(x)){
+      x <- apply(x,2,c, simplify = FALSE)
+    }
+    else{
+      stop("The data has to be a vector, a list of vectors or a matrix")
+    }
+  }
+  if(!is.list(x)) x <- as.list(x) 
+  return(x)
+}
+  
+
+kov_cross <- function(x,y,kov){ #returns the variance-kovariance-matrix 
+                                #input: list of vectors
+  m <- matrix(nrow = length(x), ncol = length(y))
+  for (i in seq_along(x)){
+    for (j in seq_along(y)){
+      m[i,j] <- kov(x[[i]],y[[j]])
+    }
+  }
+  #outer(x,y, function(x,y){mapply(kov,x,y)})
+  return(m)
+}
